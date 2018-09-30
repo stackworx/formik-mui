f1ed3701b8e65c356619523dfb46c91f977623c0
cliedeman <ciaranliedeman@gmail.com>
2018-09-30 16:10:39 +0200

    Make field props to Component part of the api

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,22 @@
-import TextField from './TextField';
-import Switch from './Switch';
-import Checkbox from './Checkbox';
-import Select from './Select';
+import TextField, { fieldToTextField } from './TextField';
+import Switch, { fieldToSwitch } from './Switch';
+import Checkbox, { fieldToCheckbox } from './Checkbox';
+import CheckboxWithLabel from './CheckboxWithLabel';
+import Select, { fieldToSelect } from './Select';
 import SimpleFileUpload from './SimpleFileUpload';
-import RadioGroup from './RadioGroup';
+import RadioGroup, { fieldToRadioGroup } from './RadioGroup';
 
-export { TextField, Switch, Checkbox, Select, SimpleFileUpload, RadioGroup };
+export {
+  TextField,
+  fieldToTextField,
+  Switch,
+  fieldToSwitch,
+  Checkbox,
+  fieldToCheckbox,
+  CheckboxWithLabel,
+  Select,
+  fieldToSelect,
+  SimpleFileUpload,
+  RadioGroup,
+  fieldToRadioGroup,
+};
