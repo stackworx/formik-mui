698c3c80b9d0eb95eb18b6d57639cb23ed8e4e4a
Keisuke Kan <9renpoto@gmail.com>
2019-07-01 04:19:59 +0900

    Fixed TextFieldProps (#85)
    * fixed variant default props

--- src/__tests__/TextField.test.tsx
+++ src/__tests__/TextField.test.tsx
@@ -2,13 +2,29 @@ import * as React from 'react';
 import { Formik, Field, Form } from 'formik';
 import renderer from 'react-test-renderer';
 
-import { TextField } from '../TextField';
+import { TextField, TextFieldProps } from '../TextField';
 
 test('TextField Renders Correctly', () => {
   const component = renderer.create(
     <Formik initialValues={{}} onSubmit={() => null}>
       <Form>
         <Field name="test" label="Text" component={TextField} />
+      </Form>
+    </Formik>
+  );
+
+  expect(component.toJSON()).toMatchSnapshot();
+});
+
+test('Wrapped TextField', () => {
+  const TextFieldComponent = (p: TextFieldProps) => (
+    <TextField variant="standard" {...p} />
+  );
+
+  const component = renderer.create(
+    <Formik initialValues={{}} onSubmit={() => null}>
+      <Form>
+        <Field name="test" label="Text" component={TextFieldComponent} />
       </Form>
     </Formik>
   );
