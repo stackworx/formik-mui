9bc877d6e687a452b100d40ab0abb8494ab4d23b
Ciaran Liedeman <ciaranliedeman@gmail.com>
2019-10-31 01:47:19 +0200

    feat: upgrade to formik 2.0

--- packages/formik-material-ui/src/__tests__/InputBase.test.tsx
+++ packages/formik-material-ui/src/__tests__/InputBase.test.tsx
@@ -1,11 +1,16 @@
 import * as React from 'react';
 import { Formik, Field, Form } from 'formik';
-import renderer from 'react-test-renderer';
+import {
+  render,
+  fireEvent,
+  waitForElementToBeRemoved,
+} from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
 
 import { InputBase } from '../InputBase';
 
 test('InputBase Renders Correctly', () => {
-  const component = renderer.create(
+  const { asFragment } = render(
     <Formik onSubmit={() => null} initialValues={{ test: 'Input' }}>
       <Form>
         <Field name="test" label="InputBase" component={InputBase} />
@@ -13,5 +18,27 @@ test('InputBase Renders Correctly', () => {
     </Formik>
   );
 
-  expect(component.toJSON()).toMatchSnapshot();
+  expect(asFragment()).toMatchSnapshot();
+  // component.root.findByProps({ type: "text" }).props.onClick();
+});
+
+test('InputBase Renders Correctly disabled', () => {
+  const { asFragment, getByTestId } = render(
+    <Formik onSubmit={() => null} initialValues={{ test: 'Input' }}>
+      <Form>
+        <Field
+          name="test"
+          inputProps={{ 'data-testid': 'input' }}
+          label="InputBase"
+          component={InputBase}
+        />
+      </Form>
+    </Formik>
+  );
+
+  waitForElementToBeRemoved;
+
+  fireEvent.change(getByTestId('input'), { target: { value: 'a' } });
+  expect(asFragment()).toMatchSnapshot();
+  // component.root.findByProps({ type: "text" }).props.onClick();
 });
